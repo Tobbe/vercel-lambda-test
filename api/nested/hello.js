65c038510774b76605a81381bd0d2e8cca7dfc15
Tobbe Lundberg <tobbe@tlundberg.com>
2021-06-22 21:26:57 +0200

    Add files

---
+++ api/nested/hello.js
@@ -0,0 +1,10 @@
+module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    url: req.url,
+    path: req.path,
+    id: 'api/nested/hello.js'
+  })
+}
